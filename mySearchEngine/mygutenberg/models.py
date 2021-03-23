ab4baed9fa82c28b62cdf85cbe579b56d89bcb29
Mylène Balech <mylenebalech@macbook-pro-de-mylene.home>
2021-03-23 09:42:52 +0100

    Création table pour stocker, bookID,URL,COVER et AUTEURS

--- mySearchEngine/mygutenberg/models.py
+++ mySearchEngine/mygutenberg/models.py
@@ -4,8 +4,10 @@ class BooksUrl(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	bookID = models.IntegerField(default='-1')
 	url = models.CharField(max_length = 1000, unique=True)
+	auteurs = models.CharField(max_length = 1000, default="")
+	cover = models.CharField(max_length = 1000,  default="")
 
 class TermesUrl(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	terme = models.CharField(max_length = 1000, unique=True)
-	urls = models.CharField(max_length = 1000, unique=True)
+	urls = models.CharField(max_length = 1000)
