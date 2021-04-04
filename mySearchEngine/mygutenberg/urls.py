583b5fcab8c3cd4de6a51d963611bf8ea553e751
MBalech <mylene.balech@gmail.com>
2021-04-04 22:50:07 +0200

    Regex OK

--- mySearchEngine/mygutenberg/urls.py
+++ mySearchEngine/mygutenberg/urls.py
@@ -4,4 +4,6 @@ from mygutenberg import views
 urlpatterns = [
 	path('books/', views.RedirectionListeDesLivres.as_view()),
 	path('book/<int:pk>/', views.LivreDetail.as_view()),
+	path('book_regex/<str:regex>/', views.RechercheRegEx.as_view()),
+	path('book_simple/<str:regex>/', views.RechercheSimple.as_view())
 ]
